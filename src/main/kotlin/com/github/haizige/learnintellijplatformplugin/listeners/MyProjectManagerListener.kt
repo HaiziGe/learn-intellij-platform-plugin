d6ebc800ade9c650b7551543e2e619072b21a4ac
GitHub Action <action@github.com>
2022-04-24 09:44:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/haizige/learnintellijplatformplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/haizige/learnintellijplatformplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.haizige.learnintellijplatformplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.haizige.learnintellijplatformplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/haizige/learnintellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.haizige.learnintellijplatformplugin.services
+
+import com.github.haizige.learnintellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
